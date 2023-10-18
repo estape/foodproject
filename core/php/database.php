806a810b4d1a1ee28a9da4846cda8f4903ffe0ca
unknown <rodrigo_estape@hotmail.com>
2023-10-18 20:29:59 -0300

    Atualização - 18/10/2023
    Atualização no arquivo database.php, adição de um algoritmo de redundância para armazenamento de dados de usuário.
    Obs: Este algoritmo deve ser usado apenas para caráter de protótipo.

--- core/php/database.php
+++ core/php/database.php
@@ -1,38 +1,20 @@
 <?php
-$servername = "127.0.0.1";
-$database = "foodproject";
-$username = "web";
-$password = "QwEr1616@@";
-
-class ConnectToDB
-{
-    public function connect()
-    {
-        global $servername, $username, $password, $database;
-        
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $database);
-        
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-
-        echo "Connected successfully";
-
-        $sql = "INSERT INTO users (name, cpf, data, cardUser) VALUES ('Test', '00000000000', '1234567899876543210')";
-        if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-
-        mysqli_close($conn);
-    }
-}
+$fullName;
+$cpf;
+$dataBirth;
+$card;
 
 class NewUserData
 {
+    // Adicione um construtor para a classe
+    public function __construct($fullName, $cpf, $dataBirth, $card)
+    {
+        $filename = $cpf . ".txt";  // Use o valor de CPF para nomear o arquivo
+        $myfile = fopen($filename, "w") or die("Unable to open file!");
 
+        $txt = "fullname=" . $fullName . "\n" . "cpf=" . $cpf . "\n" . "databirth=" . $dataBirth . "\n" . "card=" . $card;
+        fwrite($myfile, $txt);
+        fclose($myfile);
+    }
 }
 ?>
