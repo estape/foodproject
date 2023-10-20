92f56d2176ccf732befc1289b75937b22db10ed5
unknown <rodrigo_estape@hotmail.com>
2023-10-19 21:32:40 -0300

    Alteração de nome de arquivo
    Alteração dos arquivos 1.html para login.html e 3.html para signup.html

--- core/php/database.php
+++ core/php/database.php
@@ -1,27 +1,39 @@
 <?php
-class NewUserData
+class database
 {
     public function NewUserData($fullname, $cpf, $dataBirth, $card)
     {
-        $AllData = "fullname = $fullname\ncpf = $cpf\ndataBirth = $dataBirth\ncard = $card";
+        // $AllData = "fullname = $fullname\ncpf = $cpf\ndataBirth = $dataBirth\ncard = $card";
         
+        // $Path = "core/php/db/Users";
+        
+        // // Escrever a string em um novo arquivo com o nome "WriteLines.txt".
+        // file_put_contents("$Path/$cpf.txt", $AllData);
         $Path = "core/php/db/Users";
         
-        // Escrever a string em um novo arquivo com o nome "WriteLines.txt".
-        file_put_contents("$Path/$cpf.txt", $AllData);
+        $DataToJson = new stdClass();
+        $DataToJson-> name = $fullname;
+        $DataToJson-> databirth = $dataBirth;
+        $DataToJson-> card = $card;
+
+        $jsonObject = json_decode($DataToJson);
+        file_put_contents("$Path/$cpf.json", $jsonObject);
+        
     }
 
     public function LoadRestaurant($id)
     {
-        $file = fopen("$id.txt", "r");
+        // $file = fopen("$id.txt", "r");
 
-        //Output lines until EOF is reached
-        while(! feof($file)) {
-          $line = fgets($file);
-          echo $line. "<br>";
-        }
+        // //Output lines until EOF is reached
+        // while(! feof($file)) {
+        //   $line = fgets($file);
+        //   echo $line. "<br>";
+        // }
 
-        fclose($file);
+        // fclose($file);
+
+        
     }
 }
 ?>
