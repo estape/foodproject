a01b44ec4b3703ebee5c385f43ca82a4457cfa15
unknown <rodrigo_estape@hotmail.com>
2023-10-23 19:45:23 -0300

    Atualização - 23/10/2023
    Alteração no arquivo database.php

--- core/php/database.php
+++ core/php/database.php
@@ -3,12 +3,6 @@ class database
 {
     public function NewUserData($fullname, $cpf, $dataBirth, $card)
     {
-        // $AllData = "fullname = $fullname\ncpf = $cpf\ndataBirth = $dataBirth\ncard = $card";
-        
-        // $Path = "core/php/db/Users";
-        
-        // // Escrever a string em um novo arquivo com o nome "WriteLines.txt".
-        // file_put_contents("$Path/$cpf.txt", $AllData);
         $Path = "core/php/db/Users";
 
         $DataToJson = new stdClass();
@@ -16,24 +10,23 @@ class database
         $DataToJson-> databirth = $dataBirth;
         $DataToJson-> card = $card;
 
-        $jsonObject = json_decode($DataToJson);
-        file_put_contents("$Path/$cpf.json", $jsonObject);
+        $jsonUsers = json_encode($DataToJson);
+        file_put_contents("$Path/$cpf.json", $jsonUsers);
         
     }
 
     public function LoadRestaurant($id)
     {
-        $file = fopen("$id.txt", "r");
+        file_get_contents("/core/db/Local/db_restaurants.json", $jsonRT);
+        $jsonRT = json_decode($DataRT, true);
 
-        // //Output lines until EOF is reached
-        // while(! feof($file)) {
-        //   $line = fgets($file);
-        //   echo $line. "<br>";
-        // }
-
-        // fclose($file);
-
-        
+        if ($DataRT)
+        {
+            foreach($DataRT as $item)
+            {
+                echo 'name'
+            }
+        }
     }
 }
 ?>
