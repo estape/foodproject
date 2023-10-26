9463bc90adedf4f6caa90d55c347e89ff7c7cf82
juanpablitozito <146151612+juanpablitozito@users.noreply.github.com>
2023-10-26 20:20:26 -0300

    Update mobile-navbar.js

--- frontend/html/principal/mobile-navbar.js
+++ frontend/html/principal/mobile-navbar.js
@@ -1,26 +1,43 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+    constructor(mobilemenu, navlist, navlinks) {
+        this.mobilemenu = document.querySelector(mobilemenu);
+        this.navlist = document.querySelector(navlist);
+        this.navlinks = document.querySelectorAll(navlinks);
+        this.activeclass = "active";
+
+        this.handleclick = this. handleclick.bind(this);
     }
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", () => console.log
-        ("Hey"));
+
+    animateLinks() {
+        this.navlinks.forEach((link, index) => {
+            console.log(index);
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navlinkFade 0.5s ease forwards $ {index / 7 + 0.3}s`);
+        });
+    }
+
+    handleclick() {
+        this.navlist.classList.toggle(this.activeclass);
+        this.mobilemenu.classList.toggle(this.activeclass);
+        this.animateLinks();
+    }
+
+    addclickevent() {
+        this.mobilemenu.addEventListener("click", this.handleclick.bind(this));
     }
 
     init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
+        if (this.mobilemenu) {
+            this.addclickevent();
         }
         return this;
     }
 }
 
-const MobileNavbar = new MobileNavbar(
-  ".mobile-menu",
-  ".nav-list" ,
-  ".nav-list li" , 
+const mobilenavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li"
 );
-MobileNavbar.init();
+mobilenavbar.init();
