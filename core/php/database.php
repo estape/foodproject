5aa694f82a86c65c9f0189de9699e27444d2b589
unknown <rodrigo_estape@hotmail.com>
2023-11-05 22:26:22 -0300

    Atualização - 05/11/2023
    Atualização no arquivo database.php, função reserva de restaurante (save.php)

--- core/php/database.php
+++ core/php/database.php
@@ -3,8 +3,9 @@ class database
 {
     public function SetUserData($fullname, $cpf, $dataBirth, $card, $email, $password)
     {
-        $userFilePath = "core/db/Users/"; // Substitua pelo caminho correto
+        $dbFilePath = "core/db/Users/"; // Substitua pelo caminho correto
     
+        //Salva cada variável em suas respectivas propriedades
         $DataToJson = new stdClass();
         $DataToJson->name = $fullname;
         $DataToJson->databirth = $dataBirth;
@@ -13,11 +14,11 @@ class database
         $DataToJson->password = $password;
     
         // Verifica se o diretório para os arquivos dos usuários existe, se não cria
-        if (!file_exists($userFilePath)) {
-            mkdir($userFilePath, 0755, true);
+        if (!file_exists($dbFilePath)) {
+            mkdir($dbFilePath, 0755, true);
         }
     
-        $userFile = "$userFilePath/$cpf.json";
+        $userFile = "$dbFilePath/$email.json";
     
         // Tenta gravar os dados do usuário no arquivo
         if (file_put_contents($userFile, json_encode($DataToJson)) !== false) {
@@ -27,54 +28,48 @@ class database
         }
     }
 
-    public function GetUserData()
+    public function GetUserData($email, $password)
     {
-        
+        $dbFilePath = "core/db/Users/";
+
+        if(file_exists("$dbFilePath/$email"))
+        {
+            file_get_contents("$dbFilePath/$email", json_decode($JsonToData));
+
+            $userEmail = $JsonToData -> email;
+            $UserPassword = $JsonToData -> password;
+
+            //o e-mail e senha PRECISAM ser em minúsculos (case-sensitivity)
+            if ($email == $userEmail && $password == $UserPassword)
+            {
+                //Carrega os mesmos dados do arquivo json do usuário, porém não carrega a senha por questões de segurança
+                $userData = array($name, $cpf, $dataBirth, $card, $email);
+                return $userData;
+            }
+        }
+
+        else
+        {
+            return false;
+        }
+
     }
 
-    public function GetRTName()
+    public function GetRTData()
     {
-        file_get_contents("core/db/Local/db_restaurants.json", $jsonRT);
-        $jsonRT = json_decode($DataRT, true);
-        $DataRT = new stdClass();
-        $name = $DataRT -> name;
-        return $name;
-    }
+        $dbFilePath = "core/db/local/db_restaurants.json";
 
-    public function GetRTType()
-    {
-        file_get_contents("/core/db/Local/db_restaurants.json", $jsonRT);
-        $jsonRT = json_decode($DataRT, true);
-        $DataRT = new stdClass();
-        $type = $DataRT -> type;
-        return $type;
-    }
+        if(file_exists($dbFilePath))
+        {
+            file_get_contents($dbFilePath, json_decode($JsonToData));
+            return $JsonToData;
+        }
 
-    public function GetRTAdress()
-    {
-        file_get_contents("/core/db/Local/db_restaurants.json", $jsonRT);
-        $jsonRT = json_decode($DataRT, true);
-        $DataRT = new stdClass();
-        $address = $DataRT -> adress;
-        return $address;
-    }
+        else
+        {
+            return false;
+        }
 
-    public function GetRTTel()
-    {
-        file_get_contents("/core/db/Local/db_restaurants.json", $jsonRT);
-        $jsonRT = json_decode($DataRT, true);
-        $DataRT = new stdClass();
-        $tel = $DataRT -> tel;
-        return $tel;
-    }
-
-    public function GetRTStars()
-    {
-        file_get_contents("/core/db/Local/db_restaurants.json", $jsonRT);
-        $jsonRT = json_decode($DataRT, true);
-        $DataRT = new stdClass();
-        $stars = $DataRT -> stars;
-        return $stars;
     }
 }
 ?>
